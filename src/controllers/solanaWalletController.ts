74085e7cf711e49e0773535fb242ecee4a061888
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2024-05-14 22:47:10 +0700

    Update: solana wallet: select fee token

--- src/controllers/solanaWalletController.ts
+++ src/controllers/solanaWalletController.ts
@@ -1,6 +1,10 @@
 import * as anchor from "@coral-xyz/anchor";
 import { provider } from "../lib/hyper-wallet-program";
-import { gasFeeSponsor } from "../lib/gas-fee-sponsor";
+import {
+  createPayGasFeeWithUsdtTx,
+  createSponsoredTx,
+  gasFeeSponsor,
+} from "../lib/gas-fee-sponsor";
 import { Request, Response } from "express";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
 import {
@@ -35,7 +39,8 @@ export async function constructTransferLamportsTx(req: Request, res: Response) {
 }
 
 export async function constructTransferSplTx(req: Request, res: Response) {
-  const { fromAddress, toAddress, tokenMintAddress, rawAmount } = req.body;
+  const { fromAddress, toAddress, tokenMintAddress, rawAmount, feeToken } =
+    req.body;
 
   const fromAta = await getOrCreateAssociatedTokenAccount(
     provider.connection,
@@ -53,32 +58,26 @@ export async function constructTransferSplTx(req: Request, res: Response) {
     true
   );
 
-  const tx = new anchor.web3.Transaction();
-  tx.add(
-    createTransferCheckedInstruction(
-      fromAta.address,
-      new PublicKey(tokenMintAddress),
-      toAta.address,
-      new PublicKey(fromAddress),
-      rawAmount,
-      6
-    )
+  const ix = createTransferCheckedInstruction(
+    fromAta.address,
+    new PublicKey(tokenMintAddress),
+    toAta.address,
+    new PublicKey(fromAddress),
+    rawAmount,
+    6
   );
-  tx.feePayer = gasFeeSponsor.publicKey;
-  tx.recentBlockhash = (
-    await provider.connection.getRecentBlockhash()
-  ).blockhash;
-  tx.partialSign(gasFeeSponsor);
-  const serializedTx = tx.serialize({
-    verifySignatures: true,
-    requireAllSignatures: false,
-  });
-  const base64tx = serializedTx.toString("base64");
-  res.json({ base64tx });
+  if (feeToken == "USDT") {
+    console.log("🚀 ~ constructTransferSplTx ~ feeToken:", feeToken);
+    const { base64tx } = await createPayGasFeeWithUsdtTx(ix, fromAddress);
+    return res.json({ base64tx });
+  } else {
+    const { base64tx } = await createSponsoredTx(ix);
+    return res.json({ base64tx });
+  }
 }
 
 export async function constructTransferNftTx(req: Request, res: Response) {
-  const { fromAddress, toAddress, nftMintAddress } = req.body;
+  const { fromAddress, toAddress, nftMintAddress, feeToken } = req.body;
 
   const fromAta = await getOrCreateAssociatedTokenAccount(
     provider.connection,
@@ -96,26 +95,20 @@ export async function constructTransferNftTx(req: Request, res: Response) {
     true
   );
 
-  const tx = new anchor.web3.Transaction();
-  tx.add(
-    createTransferCheckedInstruction(
-      fromAta.address,
-      new PublicKey(nftMintAddress),
-      toAta.address,
-      new PublicKey(fromAddress),
-      1, // Nft amount
-      0 // Nft decimals is always 0
-    )
+  const ix = createTransferCheckedInstruction(
+    fromAta.address,
+    new PublicKey(nftMintAddress),
+    toAta.address,
+    new PublicKey(fromAddress),
+    1, // Nft amount
+    0 // Nft decimals is always 0
   );
-  tx.feePayer = gasFeeSponsor.publicKey;
-  tx.recentBlockhash = (
-    await provider.connection.getRecentBlockhash()
-  ).blockhash;
-  tx.partialSign(gasFeeSponsor);
-  const serializedTx = tx.serialize({
-    verifySignatures: true,
-    requireAllSignatures: false,
-  });
-  const base64tx = serializedTx.toString("base64");
-  res.json({ base64tx });
+  if (feeToken == "USDT") {
+    console.log("🚀 ~ constructTransferSplTx ~ feeToken:", feeToken);
+    const { base64tx } = await createPayGasFeeWithUsdtTx(ix, fromAddress);
+    return res.json({ base64tx });
+  } else {
+    const { base64tx } = await createSponsoredTx(ix);
+    return res.json({ base64tx });
+  }
 }
--- src/lib/gas-fee-sponsor.ts
+++ src/lib/gas-fee-sponsor.ts
@@ -3,6 +3,11 @@ import * as anchor from "@coral-xyz/anchor";
 import { provider } from "./hyper-wallet-program";
 // @ts-ignore
 import bs58 from "bs58";
+import {
+  createTransferCheckedInstruction,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
 const privateKey =
   "Tptte9auYxHzUNki3ACapEMoGbmeubMjm2TV7jx7hDwJptBoiet99o3abNUnDyEDLT9npJhtR5U16Ym1bedm54f";
 export const gasFeeSponsor = web3.Keypair.fromSecretKey(
@@ -13,6 +18,49 @@ export async function createSponsoredTx(
   instruction: anchor.web3.TransactionInstruction
 ) {
   const tx = new anchor.web3.Transaction();
+  tx.add(instruction);
+  tx.feePayer = gasFeeSponsor.publicKey;
+  tx.recentBlockhash = (
+    await provider.connection.getRecentBlockhash()
+  ).blockhash;
+  tx.partialSign(gasFeeSponsor);
+  const serializedTx = tx.serialize({
+    verifySignatures: true,
+    requireAllSignatures: false,
+  });
+  const base64tx = serializedTx.toString("base64");
+  return { base64tx };
+}
+
+export async function createPayGasFeeWithUsdtTx(
+  instruction: anchor.web3.TransactionInstruction,
+  sponsoredAddress: string
+) {
+  const USDT_MINT_ADDRESS = "5oUHhQyq6wgq1HnbVf5Vr3Soxu5FNU1Jv2YeKPLopg7a";
+  const fromAta = await getOrCreateAssociatedTokenAccount(
+    provider.connection,
+    gasFeeSponsor,
+    new PublicKey(USDT_MINT_ADDRESS),
+    new PublicKey(sponsoredAddress),
+    true
+  );
+  const toAta = await getOrCreateAssociatedTokenAccount(
+    provider.connection,
+    gasFeeSponsor,
+    new PublicKey(USDT_MINT_ADDRESS),
+    gasFeeSponsor.publicKey,
+    true
+  );
+  const tx = new anchor.web3.Transaction();
+  const payGasFeeWithUsdtIx = createTransferCheckedInstruction(
+    fromAta.address,
+    new PublicKey(USDT_MINT_ADDRESS),
+    toAta.address,
+    new PublicKey(sponsoredAddress),
+    0.000015 * Math.pow(10, 6),
+    6
+  );
+  tx.add(payGasFeeWithUsdtIx);
   tx.add(instruction);
   tx.feePayer = gasFeeSponsor.publicKey;
   tx.recentBlockhash = (
