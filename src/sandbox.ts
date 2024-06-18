8da67fd58ce175795ba02ec7f5c32649f7a0bc98
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2024-06-18 13:31:06 +0700

    Fix

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,7 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
 import { SolanaParser } from "@debridge-finance/solana-transaction-parser";
 import hyperWalletProgramIdl from "./lib/hyper-wallet-program/idl/hyper_wallet_program.json";
-import { provider } from "./lib/hyper-wallet-program";
 import * as anchor from "@coral-xyz/anchor";
 
 export async function test() {}
